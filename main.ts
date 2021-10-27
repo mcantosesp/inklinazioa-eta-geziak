8b8d7f31e4297a31b0acee0b0b9fc5f7ef71e3c6
mcantosesp <93263251+mcantosesp@users.noreply.github.com>
2021-10-27 14:28:14 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.South)
+        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.South)
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+})
+basic.forever(function () {
+	
+})
